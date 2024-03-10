83c70704691a6b168b31fa320dc881fbde7acbad
DoanPham <doananhphamuk@gmail.com>
2024-03-10 13:00:03 +0700

    Git checkout main

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -3,13 +3,17 @@ import "./custom.css";
 import {  Route, Routes } from "react-router-dom";
 import { LoginApp } from "components/Pages/Management/Login/login";
 import { ManagementApp } from "components/Pages/Management";
+import { AdminPageApp } from "components/Pages/Admin";
+import { LandingPageApp } from "components/Pages/Common";
 
 export const App: React.FC = () => {
   return (
     <>
       <Routes>
         <Route path="/management/*" element={<ManagementApp />} />
-        <Route path="/" element={<LoginApp />} />
+        <Route path="/admin/*" element={<AdminPageApp />} />
+        <Route path="/" element={<LandingPageApp />} />
+        <Route path="/login" element={<LoginApp />} />
       </Routes>
     </>
   );
